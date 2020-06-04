95559e09b754dad42ff94a8d708116fbf5f472bf
SinaSystem <sinasystem47@gmail.com>
2020-06-05 00:44:05 +0430

    Initial commit

---
+++ app/src/main/java/com/androidtutz/anushka/didemo/SmartPhoneComponent.java
@@ -0,0 +1,10 @@
+package com.androidtutz.anushka.didemo;
+
+import dagger.Component;
+
+@Component(modules = {MemoryCardModule.class,NCBatteryModule.class})
+public interface SmartPhoneComponent {
+
+    SmartPhone getSmartPhone();
+
+}
